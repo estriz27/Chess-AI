3d1be77e2af4e872fb739c4050be098578af8d72
Elias <ebs021@bucknell.edu>
2017-05-01 23:02:20 -0400

    added ability to choose type of ai, and completed comments on every function. In addition, code was cleaned up

--- ChessGame.py
+++ ChessGame.py
@@ -4,6 +4,8 @@ import time
 import random
 from Node import Node
 
+#This program models a game of chess with the help of ChessNut, and integrates a GUI and intelligent chess agent
+#Authors: Elias Strizower, Henry Kwan, Alex Godziela
 
 ################ GLOBAL VARIABLES ########################################
 
@@ -11,21 +13,11 @@ board = ChessBoard(8,8)
 ## chessgame = Game() # use this for a NEW GAME, use a fen generator for testing
 chessgame = Game()
 
-HumanNextMoves = {}
-AINextMoves = {}
-
-##chessgame = Game(fen='rnbqkbnr/p1pppppp/8/1p6/2P5/8/PP1PPPPP/RNBQKBNR w KQkq b6 0 2')
-##board = ChessBoard(8,8)
+HumanNextMoves = {} #Dictionary that will hold 2 ply of the human player's next moves
+AINextMoves = {} #Dictionary that will hold 2 ply of the AI's next moves
 board.updateBoard(str(chessgame))
 
-
-"""
---iniitial moves--
-['a2a3', 'a2a4', 'b2b3', 'b2b4', 'c2c3', 'c2c4', 'd2d3', 'd2d4', 'e2e3',
- 'e2e4', 'f2f3', 'f2f4', 'g2g3', 'g2g4', 'h2h3', 'h2h4', 'b1c3', 'b1a3',
- 'g1h3', 'g1f3']
-"""
-
+#This functions checks whether a player is in check, checkmate, or stalemate
 def checkStatus():
     if chessgame.status == 1:
         print("CHECK")
@@ -35,16 +27,17 @@ def checkStatus():
     if chessgame.status ==3:
         print("STALEMATE")
 
+#This function prints the player whose current turn it is
 def printplayer():
     if chessgame.state[0] == 'w':
         return "White"
     else:
         return "Black"
 
-
+#This function implements the minimax algorithm
 def minimax(node, depth, maximizingPlayer):
     if depth == 0 or node.leaf == True:
-        heuristicValue = findBestMove()  #best move for AI
+        heuristicValue = findBestMove('b')  #best move for AI
         return heuristicValue
 
     #maximizingPlayer -AI
@@ -65,8 +58,8 @@ def minimax(node, depth, maximizingPlayer):
 
 
 #this function is simply a heuristic to get the move with the highest point value
-def findBestMove():
-    possible_moves = chessgame.get_moves('b')
+def findBestMove(player):
+    possible_moves = chessgame.get_moves(player)
 
     #updates dictionary
     piece_values = {'p': 1, 'b': 3, 'n': 3, 'r': 5, 'q': 9, 'k': 200, ' ': 0}
@@ -123,7 +116,7 @@ def findBestMove():
 # return str(bestMove)
 '''
 
-
+#This function generates a dictionary where the keys are the 1 ply moves, and the value at the key are the 2 ply moves that stem from the key move
 def lookAhead(player):
     #create dictionary for 2-ply moves
     secondLayerMoves = {}
@@ -135,14 +128,11 @@ def lookAhead(player):
         secondLayerMoves[move] = []
     for move in chessgame.get_moves(player):
         chessgame.apply_move(move)
-#        if player == 'b':
-#            opponent = 'w'    This commented code isn't going to work and will break the program
-#        else:
-#            opponent = 'b'
         secondLayerMoves[move] += chessgame.get_moves(player)
         chessgame.set_fen(currentFen)
     return secondLayerMoves
 
+#This function is a helper function that is used to change the player from 'b' to 'w' in the FEN. It is neccesary to lookahead 2 ply since ChessNut presents issues in doing so
 def changeFenWB(lookAheadFen):
     for x in range(5,20):
         if lookAheadFen[-x] == 'b' and lookAheadFen[-x+1] == ' ' and lookAheadFen[-x-1] == ' ':
@@ -150,7 +140,7 @@ def changeFenWB(lookAheadFen):
             break
     return lookAheadFen
 
-#returns point value for specific move, used for white player only
+#This function returns point value for specific move, used for white player only
 def findMoveValue(move):
     piece_values = {'p': 1, 'b': 3, 'n': 3, 'r': 5, 'q': 9, 'k': 200, ' ': 0}
     space_moving_to = move[2:] # 'a6'
@@ -159,7 +149,7 @@ def findMoveValue(move):
     pieceValue = piece_values[piece] #int
     return pieceValue
 
-#sounds confusing, but finds key (move) based on value (points)
+#This function finds a key (move) based on value (points)
 def findMoveBasedonValue(bestValue, player):
     piece_values = {'p': 1, 'b': 3, 'n': 3, 'r': 5, 'q': 9, 'k': 200, ' ': 0}
 
@@ -176,7 +166,42 @@ def findMoveBasedonValue(bestValue, player):
         if moves_dict[key] == bestValue:
             return key
 
+#This function implements a minimax AI to play against       
+def MinimaxAgent():
+        ourPossibleMoves = chessgame.get_moves('b')
+        for move in ourPossibleMoves:
+            originNode = Node(move)
+            for key in HumanNextMoves: 
+                originNode.children = HumanNextMoves[key]  
+            originNode.setIsLeaf() #sets isLeaf equal to false or true depending on if node has children
+            bestValue = minimax(originNode, 2, True)
+            bestMove = findMoveBasedonValue(bestValue, 'b')
+        move = bestMove
+        return bestMove
+    
+#This function implements a 1 ply random move AI to play against 
+def RandomMoveAgent():
+     ourPossibleMoves = chessgame.get_moves('b')
+     move = random.choice(ourPossibleMoves)
+     return move
 
+#This function implements a 1 ply best move AI to play against 
+def BestMoveAgent():
+     ourPossibleMoves = chessgame.get_moves('b')
+     bestMoveValue = findBestMove('b')
+     bestMove = findMoveBasedonValue(bestMoveValue,'b')
+     return bestMove
+
+#This function is called to determine the correct agent that the user selected to play against
+def agentMove(agent):
+    if agent == "1":
+        return RandomMoveAgent()
+    elif agent == "2":
+        return BestMoveAgent()
+    elif agent == "3":
+        return MinimaxAgent()
+
+#This function runs the sets up the game and runs it by assigning an ai, taking user input as moves, and displayin the GUI
 def runGame():
     global HumanNextMoves
     global AINextMoves
@@ -191,6 +216,21 @@ def runGame():
     print('Turn ' + str(turn_counter) + '\n')
     print("White Score: " +  str(whiteValue))
     print("Black Score: " +  str(blackValue) + '\n')
+
+    #Determine what chess agent the player wants to play against
+    agent = input("What chess agent do you want to play against?(1 = easy (1 Ply Random Move) , 2 = medium (1 Ply Best Move), 3 = hard (2 Ply Minimax)\n")
+    if agent == "1" or agent == "2" or agent == "3":
+        pass
+    else:
+        
+        while True:
+            agent = str(input("Error: Please Choose Agent [1 = easy (1 Ply Random Move) , 2 = medium (1 Ply Best Move), 3 = hard (2 Ply Minimax)]\n"))
+            if agent == "1":
+                break
+            elif agent == "2":
+                break
+            elif agent == "3":
+                break
     print(board)
 
 
@@ -214,31 +254,26 @@ def runGame():
 
 
         #white player (user) makes move
-        whiteValue += findMoveValue(move)
-        chessgame.apply_move(move)
-        board.updateBoard(str(chessgame))
+        whiteValue += findMoveValue(move) #Keeps track of how many points the white (human) player has
+        chessgame.apply_move(move) #Applies the move to the board
+        board.updateBoard(str(chessgame)) #Updates the GUI
 
 
 
-        #this handles setting humanNextMoves DONT COMMENT IT!!!
+        #this handles setting humanNextMoves. ChessNut makes it very difficult to lookahead after you make your turn. This gets around that issue by saving the FEN, creating an artificial fen to get next moves, then resetting the original FEN
         currFen = chessgame.get_fen()
         lookAheadFen = chessgame.get_fen() #user's fen if user's move is made
         lookAheadFen = list(lookAheadFen)
         changeFenWB(lookAheadFen)
-
         lookAheadFen = "".join(lookAheadFen)  #recreates fen
-
         chessgame.set_fen(lookAheadFen)
-        #
-        HumanNextMoves = lookAhead('w')
+        HumanNextMoves = lookAhead('w') #generates a dictionary with 2 ply of the next possible moves
         chessgame.set_fen(currFen)
 
         #----------------------AI MOVE-------------------------
         #after user makes move, AI needs to lookAhead
         #AI is the black player
-        lookAhead('b')
-
-
+        lookAhead('b') #generates a dictionary with 2 ply of the next possible moves
         AINextMoves = lookAhead('b')
         board.updateBoard(str(chessgame))
         turn_counter += 1
@@ -246,34 +281,15 @@ def runGame():
         print("White Score: " +  str(whiteValue))
         print("Black Score: " +  str(blackValue) + '\n')
         print(board)
-
-
-
-        checkStatus()
+        checkStatus() #checks whether there is a check, checkmate, or stalemate, and prints the corresponding message
         print("AI is thinking...\n")
 
-        ourPossibleMoves = chessgame.get_moves('b')
-        #call minimax HERE
-        for move in ourPossibleMoves:
-            originNode = Node(move)
-            for key in HumanNextMoves:  ##WHAT DOES THIS DO? Please look
-                originNode.children = HumanNextMoves[key]  ###WHAT IS THE PURPOSE OF THIS LINE. Should it not be +=?
-            #Previous code:
-            #for humanMove in chessgame.get_moves('w'):
-            #       originNode.children = HumanNextMoves[humanMove]
-
-            originNode.setIsLeaf() #sets isLeaf equal to false or true depending on if node has children
-            #minimax(originMove, depthLevel, maximizingPlayer)
-
-            bestValue = minimax(originNode, 2, True)  #passed in Node *CHANGED*
-            bestMove = findMoveBasedonValue(bestValue, 'b')
-        move = bestMove
-        blackValue += findMoveValue(move)
-        #######move = random.choice(chessgame.get_moves())
-        ##print('got past findbestMove!')
-        ##print(move) ##### move should look something like this 'd7d6'
-        chessgame.apply_move(move)
-        board.updateBoard(str(chessgame))
+        
+        move = agentMove(agent)  #AI move based on what agent was selected
+    
+        blackValue += findMoveValue(move)  #Keeps track of how many points the black (AI) player has
+        chessgame.apply_move(move) #Applies the move to the board
+        board.updateBoard(str(chessgame)) #Updates the GUI
         turn_counter += 1
         print('Turn ' + str(turn_counter) + ' - black (AI) made move ' + move + '\n')
         print("White Score: " +  str(whiteValue))
